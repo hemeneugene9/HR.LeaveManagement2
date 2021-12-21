5f001e06215e090cf5905974f908f976e1e9cd34
hemen eugene <hemeneugene@yahoo.com>
2021-12-21 04:15:31 -0800

    JwtSettingModified

--- HR.LeaveManagement.Application/Models/Identity/JwtSettings.cs
+++ HR.LeaveManagement.Application/Models/Identity/JwtSettings.cs
@@ -9,6 +9,6 @@ namespace HR.LeaveManagement.Application.Models.Identity
         public string Key { get; set; }
         public string Issuer { get; set; }
         public string Audience { get; set; }
-        public double DurationInMunites { get; set; }
+        public double DurationInMinutes { get; set; }
     }
 }
--- HR.LeaveManagement.Identity/Services/AuthService.cs
+++ HR.LeaveManagement.Identity/Services/AuthService.cs
@@ -126,7 +126,7 @@ namespace HR.LeaveManagement.Identity.Services
                 issuer: _jwtSettings.Issuer,
                 audience: _jwtSettings.Audience,
                 claims: claims,
-                expires: DateTime.UtcNow.AddMinutes(_jwtSettings.DurationInMunites),
+                expires: DateTime.UtcNow.AddMinutes(_jwtSettings.DurationInMinutes),
                 signingCredentials: signingCredentials);
             return jwtSecurityToken;
         }
